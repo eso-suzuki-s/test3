e87e261c0ea4513e766d405bf8c873fbe3280f69
suzuki-s <s_suzuki@e-state.ne.jp>
2025-04-02 17:13:54 +0900

    test250402002/index.vue

---
+++ pages/test250402002/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <UiPageHeader :subject="response.details.subject" subheading="test20250402002" />
+    <article class="c-article">
+      <div class="l-container--large l-container--contents">
+        <div v-html="response.details.contents"></div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const { data: response } = await useFetch(
+  `${config.public.kurocoApiDomain}/rcms-api/1/content/details/test20250402002`,
+  {
+    server: false,
+  }
+);
+</script>
